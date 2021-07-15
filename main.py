cf5edbe25af0e9fa6f8ef6636ad4d4c15ce4ba85
Macdeini <84424393+Macdeini@users.noreply.github.com>
2021-07-15 14:19:44 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+import math
+
+t = turtle.Turtle()
+
+# def draw_triangle(l):
+#     for drawing in range(3):
+#         turtle.forward(l)
+#         turtle.left(120)
+#
+#
+# def draw() -> None:
+#     l = 100
+#     counter = 1
+#     hit_top = False
+#     while True:
+#         for num_of_triangles in range(counter):
+#             draw_triangle(l)
+#             t.penup()
+#             turtle.setposition(turtle.position()[0] + l/2, turtle.position()[1] + math.sqrt(l**2 - (l ** 2) / 4))
+#             t.pendown()
+#         turtle.home()
+#         l /= 2
+#         counter *= 2
+#
+#
+# if __name__ == '__main__':
+#     draw()
